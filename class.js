3d9194138088559e361fe2d4358cf455ee4c4f72
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-02 13:32:47 +0600

    Class

---
+++ class.js
@@ -0,0 +1,13 @@
+class student {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.university = "BRAC University";
+    }
+}
+
+const student1 = new student(18301041, "Eftykhar Rahman");
+const student2 = new student(18301040, "Mansura Akter");
+const student3 = new student(18301039, "Zahin Rydha");
+
+console.log(student1);
