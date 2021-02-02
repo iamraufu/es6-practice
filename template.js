d0938ea4799ee1fe01e56f596e2be9f4631af5a6
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-02 12:12:50 +0600

    ES6 Template

---
+++ template.js
@@ -0,0 +1,17 @@
+const firstName = "Raufu";
+const lastName = "Prezens";
+
+const fullName = firstName + " " + lastName + " is learning ES6";
+const fullName2 = `${firstName} ${lastName} is learning ECMASCRIPT 2015`;
+const fullName3 = `${firstName} is ${10+10+1} years old`;
+
+console.log(fullName);
+console.log(fullName2);
+console.log(fullName3);
+
+const multiLine = "JavaScript\n" + "ES6\n" + "React JS";
+const multiLine2 = `JavaScript
+ES6 
+React JS`;
+console.log(multiLine);
+console.log(multiLine2);
