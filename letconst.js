0b604dc6d03fd0f724eaa6f1d22bd60278306fa4
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-02 11:54:24 +0600

    let const

---
+++ letconst.js
@@ -0,0 +1,25 @@
+const name = "Raufu";
+
+// name = "Md. Raufu";
+
+name2 = "Raufu Prezens";
+console.log(name);
+console.log(name2);
+
+const numbers = [2, 23, 12, 15];
+numbers[1] = 20;
+numbers.push(12);
+// numbers = ["Raufu", "Prezens", "Eftykhar"];
+const player = {
+    name: "SAH",
+    jersey: 75
+};
+
+let patientName = "Rahim";
+patientName = "Karim";
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum += i;
+}
