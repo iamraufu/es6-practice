0638c9a38e4fa623b94a2dfe2f03de4f2e6796c0
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-03 03:16:44 +0600

    Destructure

--- destructure.js
+++ destructure.js
@@ -7,14 +7,20 @@ const person = {
 }
 console.log(person.favoriteSeries);
 
+const { status, age, salary } = person;
+const job = person.job;
+console.log(job, salary, status, age);
+
 const youtubers = ["mr. beast", "pewdiepie", "ninja", "dude perfect"];
-const [best, ...betters] = youtubers;
-console.log(best);
+const [best, rest, ...betters] = youtubers;
+console.log(best, rest);
 console.log(betters);
 
-// const complexObject = {
-//     info {
-//         address: 'Dhaka',
-//         country: 'Bangladesh'
-//     }
-// }
+const complexObject = {
+    name: 'abc',
+    info: {
+        address: 'dhaka',
+        country: 'Bangladesh'
+    }
+}
+const { leader } = complexObject.info;
