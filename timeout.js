4b6be806cad20a716f88f1b21c1ae74e3f455d22
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-08 17:07:08 +0600

    Timeout Timeinterval

---
+++ timeout.js
@@ -0,0 +1,9 @@
+function doSomething() {
+    console.log(123);
+}
+console.log(2220)
+setTimeout(doSomething, 2000);
+console.log(4444)
+setInterval(() => {
+    console.log("Whats up Bitch")
+}, 3000)
